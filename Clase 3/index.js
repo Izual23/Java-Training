fdd6f3487ea5b6318d5655a19340f9a16abab695
Michel Ramirez <Izual@hotmail.com.ar>
2022-06-29 21:32:27 -0300

    Training .For

--- Clase 3/index.js
+++ Clase 3/index.js
@@ -9,3 +9,17 @@ for (let index = 0; index <= 10; index++){
 
 */
 
+for (let Room = 1; Room < 11; Room++){
+    let Name = prompt("Hello, my name is...");
+    let Gender = prompt("My gender is... (Specifiy Male or Female)").toLowerCase();
+    let Service = `Your room is the number ${Room}.`;
+    if (Gender == "male"){
+        alert(Service + " " + "Please Mr. " + Name + ", be careful on the way to your room");
+    }
+    else if (Gender == "female"){
+        alert(Service + " " + "Please Ms. " + Name + ", be careful on the way to your room");
+    }
+    else{
+        alert("Please, specify your data again");
+    }
+}
