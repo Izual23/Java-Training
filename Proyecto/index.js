26e5c5ba4a9af3cad6faffcd825436956e3a4e31
Michel Ramirez <Izual@hotmail.com.ar>
2022-07-12 21:23:15 -0300

    Arrays_Objects_Fors

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -1,55 +1,55 @@
+alert("Bienvenido/a a mi página web de dibujos, a continuación podrás seleccionar tu paquete y verificar sus precios");
+alert("Haz tu pedido escribiendo el número correspondiente a cada paquete.");
 //Helpers
 let error = "Error al no seleccionar envio correctamente, por favor, realice el pedido nuevamente";
 let question = "¿Deseas envio físico? El envío físico tiene un costo de 1000 pesos argentinos.";
-
 //Calculations
 const plus = (a, b) =>  a + b;
 const minus = (a, b) =>  a - b;
 const iva = x => x * 0.40;
-
 //Details
 const prices = [
-    {id: 1, name: "Cuerpo Entero", price: 3500},
-    {id: 2, name: "Medio Cuerpo", price: 2000},
-    {id: 3, name: "Cabeza", price: 1500},
-    {id: 4, name: "Fondo", price: 3000},
-    {id: 5, name: "Chibi", price: 1000},
-    {id: 6, name: "Discount", price: 200},
-    {id: 7, name: "Sends", price: 1000},
+    {id: 1, name: "Cuerpo Entero", price: 3500, show: true},
+    {id: 2, name: "Medio Cuerpo", price: 2000, show: true},
+    {id: 3, name: "Cabeza", price: 1500, show: true},
+    {id: 4, name: "Fondo", price: 3000, show: true},
+    {id: 5, name: "Chibi", price: 1000, show: true},
+    {id: 6, name: "Discount", price: 200, show: false},
+    {id: 7, name: "Sends", price: 1000, show: false},
 ];
-
+let choose = "Elija una opcion: \n";
+for (let i = 0; i < prices.length; i++){
+    if (prices [i].show == true){
+        choose += `${prices[i].id} ${prices[i].name}: `;
+        choose += `$${prices[i].price}\n`;
+    }
+}
+let choosing = parseInt(prompt (choose));
 //Calculating 1
 let finalPaq1 = minus (plus (prices[0].price, iva(prices[0].price)), prices[5].price);
 let finalPaq2 = minus (plus (prices[1].price, iva(prices[1].price)), prices[5].price);
 let finalPaq3 = minus (plus (prices[2].price, iva(prices[2].price)), prices[5].price);
 let finalPaq4 = minus (plus (prices[3].price, iva(prices[3].price)), prices[5].price);
 let finalPaq5 = minus (plus (prices[4].price, iva(prices[4].price)), prices[5].price);
-
 //Calculating for extra
 let totalPaq1 = plus (finalPaq1, prices[6].price);
 let totalPaq2 = plus (finalPaq2, prices[6].price);
 let totalPaq3 = plus (finalPaq3, prices[6].price);
 let totalPaq4 = plus (finalPaq4, prices[6].price);
 let totalPaq5 = plus (finalPaq5, prices[6].price);
-
-alert("Bienvenido/a a mi página web de dibujos, a continuación podrás seleccionar tu paquete y verificar sus precios");
-alert("Haz tu pedido escribiendo el número correspondiente a cada paquete.");
-let choose = parseInt(prompt(`(1).${prices[0].name}: $${prices[0].price}, (2).${prices[1].name}: $${prices[1].price}, (3).${prices[2].name}: $${prices[2].price}, (4).${prices[3].name}: $${prices[3].price}, (5).${prices[4].name}: $${prices[4].price}`));
-
 //Waiting time
 function total(){
     alert("Calculando precios + ivas - descuentos. Un momento por favor");
 }
 total();
-
 //Total price
-switch (choose){
+switch (choosing){
     case 1: 
         alert(`El precio por tu pack es de ${finalPaq1} pesos argentinos.`);
         let again1 = prompt(question).toLowerCase();
         if (again1 == "si"){
-            alert(`El precio por 1: ${categories[0]} + envio físico es de ${totalPaq1} pesos argentinos.`);
-            alert("Será derivado a PayPal para realizar el pago.")
+            alert(`El precio por 1: ${prices[0].name} + envio físico es de ${totalPaq1} pesos argentinos.`);
+            alert("Será derivado a PayPal para realizar el pago.");
         }
         else if (again1 == "no"){
             alert(`Precio final: $${finalPaq1}. Será derivado a PayPal para realizar el pago.`);
@@ -62,7 +62,7 @@ switch (choose){
         alert(`El precio por tu pack es de ${finalPaq2} pesos argentinos.`);
         let again2 = prompt(question).toLowerCase();
         if (again2 == "si"){
-            alert(`El precio por 2: ${categories[1]} + envio físico es de ${totalPaq2} pesos argentinos.`);
+            alert(`El precio por 2: ${prices[1].name} + envio físico es de ${totalPaq2} pesos argentinos.`);
             alert(error)
         }
         else if (again2 == "no"){
@@ -76,7 +76,7 @@ switch (choose){
         alert(`El precio por tu pack es de ${finalPaq3} pesos argentinos.`);
         let again3 = prompt(question).toLowerCase();
         if (again3 == "si"){
-            alert(`El precio por 3: ${categories[2]} + envio físico es de ${totalPaq3} pesos argentinos.`);
+            alert(`El precio por 3: ${prices[2].name} + envio físico es de ${totalPaq3} pesos argentinos.`);
             alert("Será derivado a PayPal para realizar el pago.")
         }
         else if (again3 == "no"){
@@ -90,7 +90,7 @@ switch (choose){
         alert(`El precio por tu pack es de ${finalPaq4} pesos argentinos.`);
         let again4 = prompt(question).toLowerCase();
         if (again4 == "si"){
-            alert(`El precio por 4: ${categories[3]} + envio físico es de ${totalPaq4} pesos argentinos.`);
+            alert(`El precio por 4: ${prices[3].name} + envio físico es de ${totalPaq4} pesos argentinos.`);
             alert("Será derivado a PayPal para realizar el pago.")
         }
         else if (again4 == "no"){
@@ -104,7 +104,7 @@ switch (choose){
         alert(`El precio por tu pack es de ${finalPaq5} pesos argentinos.`);
         let again5 = prompt(question).toLowerCase();
         if (again5 == "si"){
-            alert(`El precio por 5: ${categories[4]} + envio físico es de ${totalPaq5} pesos argentinos.`);
+            alert(`El precio por 5: ${prices[4].name} + envio físico es de ${totalPaq5} pesos argentinos.`);
             alert("Será derivado a PayPal para realizar el pago.")
         }
         else if (again5 == "no"){
@@ -115,45 +115,3 @@ switch (choose){
         }
     break;
 }
-
-
-/*class Paq1{
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
-        this.sell = false;
-    }
-    sold(){
-        this.sell = true;
-    }
-}
-
-const product1 = new Paq1("Cuerpo Entero", 3500);
-product1.sold();
-
-if (product1.sell){
-    alert("Producto agregado al carro");
-}
-else{
-    alert("Producto no vendido");
-}*/
-
-//...............................................
-/*class Details{
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
-    }
-    iva(){
-        this.price = this.price * 0.40
-    }
-    discount(){
-        this.price = this.price - 200
-    }
-}
-const drawings = [];
-drawings.push(new Details(`${categories[0]}`, `${costs[0]}`))
-drawings.push(new Details(`${categories[1]}`, `${costs[1]}`))
-drawings.push(new Details(`${categories[2]}`, `${costs[2]}`))
-drawings.push(new Details(`${categories[3]}`, `${costs[3]}`))
-drawings.push(new Details(`${categories[4]}`, `${costs[4]}`))*/
